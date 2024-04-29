f30ec8094aec66645a639b3d957587cf0d2644e5
DamianTocev <84261493+DamianTocev@users.noreply.github.com>
2024-04-29 12:42:54 +0200

    Add files via upload

---
+++ array_print_elements_without_loops.php
@@ -0,0 +1,9 @@
+<?php
+$people = ["Ivan", "Branko", "Tanya", "Hristo", "Nikolay", "Petko"];
+echo $people[0] . ", ";
+echo $people[1] . ", ";
+echo $people[2] . ", ";
+echo $people[3] . ", ";
+echo $people[4] . ", ";
+echo $people[5];
+?>
